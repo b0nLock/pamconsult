0c30eaa124df6d485c1d5cc28d1a07ab1282996e
b0nLock <b1n13eaker@gmail.com>
2025-09-25 13:39:14 +0300

    ...

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const burgerBtn = document.querySelector('.header__burger');
+const headerLinks = document.querySelector('.header__links');
+const headerLink = document.querySelectorAll('.header__link');
+
+burgerBtn.addEventListener('click', () => {
+    headerLinks.classList.toggle('active');
+})
+
+headerLink.forEach(link => {
+    link.addEventListener('click', () => {
+        if (headerLinks.classList.contains('active')) headerLinks.classList.toggle('active');
+    })
+})
